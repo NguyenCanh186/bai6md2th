892697c16b7c4f34761fba6c165e618285d234e4
Canh.Nguyen <leagueyasuo186@gmail.com>
2022-02-14 11:10:10 +0700

    k

---
+++ src/TestRectangle.java
@@ -0,0 +1,8 @@
+public class TestRectangle {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+        rectangle = new Rectangle(3, 4, "yellow", false);
+        System.out.println(rectangle);
+    }
+}
---
+++ src/TestSquare.java
@@ -0,0 +1,10 @@
+public class TestSquare {
+    public static void main(String[] args) {
+        Square square = new Square();
+        System.out.println(square);
+        square = new Square(4);
+        System.out.println(square);
+        square = new Square(5, "red", false);
+        System.out.println(square);
+    }
+}
